18adb923ba93d82b414515220a68322804b05e0e
Himanshu Kumar <himanshukumar51667@gmail.com>
2026-08-07 23:02:13 +0530

    refactor: simplify sum calculation using formula

--- src_py/pythonDSA/sumofNatural.py
+++ src_py/pythonDSA/sumofNatural.py
@@ -1,5 +1,8 @@
 number = 10
-sum = 0
-for i in range(1, number + 1):
-    sum += i
-print("The sum of the first", number, "natural numbers is:", sum)
+# sum = 0
+# for i in range(1, number + 1):
+#     sum += i
+# print("The sum of the first", number, "natural numbers is:", sum)
+
+
+print(number * (number + 1) // 2)  # using the formula n(n + 1)/2
