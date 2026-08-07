db02d9b757cc313a69d1531ef72e749ee8a6c3da
Himanshu Kumar <himanshukumar51667@gmail.com>
2026-08-07 23:03:46 +0530

    refactor: replace iterative sum calculation with formula for efficiency

--- src/javaDSA/sumofNatural.java
+++ src/javaDSA/sumofNatural.java
@@ -2,11 +2,14 @@ package src.javaDSA;
 
 public class sumofNatural {
     public static void main(String[] args) {
-        int sum = 0;
-        for (int i = 1; i <= 100; i++) {
-            sum += i;
-        }
-        System.out.println("The sum of the first 100 natural numbers is: " + sum);
+        // int sum = 0;
+        // for (int i = 1; i <= 100; i++) {
+        //     sum += i;
+        // }
+        // System.out.println("The sum of the first 100 natural numbers is: " + sum);
+
+        int number = 10;
+        System.out.println("The sum of the first " + number + " natural numbers is: " + (number * (number + 1) / 2)); // using the formula n(n + 1)/2
 
     }
 }
