74e3f47f3c7ff0dad19e8be0004f760f8fa548d3
Himanshu Kumar <himanshukumar51667@gmail.com>
2026-08-09 15:56:32 +0530

    feat: add program to determine the greatest of two numbers in Java and Python

---
+++ src_py/pythonDSA/greatestoftwoNum.py
@@ -0,0 +1,10 @@
+num1 = 10
+
+num =20
+
+if num1 > num:
+    print(num1, "is greater than", num)
+elif num1 < num:
+    print(num, "is greater than", num1)
+else:
+    print("Both numbers are equal.")
